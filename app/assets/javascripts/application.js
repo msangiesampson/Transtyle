ce3ef475bd700fd631d9dee70487c5e451577ffc
Despo Pentara <despo@despo.me>
2015-03-28 15:44:42 +0000

    plug in masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
+//= require masonry.pkgd.min
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
---
+++ app/assets/javascripts/outfits.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  var container = document.querySelector('#outfits');
+  var msnry = new Masonry( container, {
+    // options
+      itemSelector: '.outfit'
+  });
+
+})
